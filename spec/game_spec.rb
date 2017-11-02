5ef038cf94917ef0a2558bbf944f7158aec0cb5c
Adrian Pastur <adrian.pastur@sage.com>
2017-11-02 15:28:20 +0000

    Add player name/ score/ update score

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,16 +6,22 @@ require 'pry'
 describe 'a player' do
   it 'has a name' do
     game = Game.new
-    game.set_player1("Adrian", 0)
-    expect(game.get_player1).to eq("Adrian",0)
+    game.set_player1 "Adrian"
+    expect(game.get_player1).to eq("Adrian")
+
   end
-  # it 'has a score' do
-  #   game.set_player1 "adrian"
-  #   expect(game.get_player1).to eq("Adrian", [0])
-  # end
+  it 'has a score' do
+    game = Game.new
+    game.set_score1 0
+    expect(game.get_score1).to eq([0])
+  end
 end
 
-# describe 'the score' do
-#   it 'starts with 0'
-#   it 'can be increased'
-# end
+describe 'the score' do
+  # it 'starts with 0'
+  it 'can be increased' do
+    game = Game.new
+    game.set_score1 0
+    expect(game.score_update 1).to eq([0,1])
+  end
+end
